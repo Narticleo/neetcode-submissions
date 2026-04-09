53cd52e93ef206a89b292f6ab458c151822143bd
Narticleo <122687447+Narticleo@users.noreply.github.com>
2026-04-09 20:09:23 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3)return n;
+        vector<int> comb(n+1, 0);
+        comb[0] = 1;
+        comb[1] = 1;
+        for(int i=2; i<=n; i++)
+            comb[i] = comb[i-1] + comb[i-2];
+        return comb[n];         
+    }
+};
