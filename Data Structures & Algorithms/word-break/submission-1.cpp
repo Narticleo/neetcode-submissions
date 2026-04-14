c5ada57a7e8f54bd653ae5f6203c22400d5058a8
Narticleo <122687447+Narticleo@users.noreply.github.com>
2026-04-14 17:43:23 +0800

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int wordLen = 0;
+        int ss = s.size();
+        int ds = wordDict.size();
+        vector<bool> isValid(ss+1, false);
+        unordered_set<string> dict;
+        for(string& word : wordDict){
+            dict.insert(word);
+            wordLen = max(wordLen, int(word.size()));
+        }        
+        isValid[0] = true;
+        for(int i=1; i<=ss; i++){
+            for(string& word : wordDict){
+                int pre = i - word.size();
+                if(i-pre >= 0 && isValid[pre] && s.substr(pre, word.size()) == word)
+                    isValid[i] = true;
+            }
+        }
+        return isValid[ss];
+    }
+};
